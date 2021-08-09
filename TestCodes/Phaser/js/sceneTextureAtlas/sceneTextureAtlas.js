e376cb4833db2e386eec544801ae9887a0a3266a
miel <keandi2@nate.com>
2021-08-09 16:07:43 +0900

    spriteJsonGenerator 적용해보기

--- TestCodes/Phaser/js/sceneTextureAtlas/sceneTextureAtlas.js
+++ TestCodes/Phaser/js/sceneTextureAtlas/sceneTextureAtlas.js
@@ -1,4 +1,6 @@
 class SceneTextureAtlas extends SceneMenuBase {
+    #_PV = {};
+
     // ctor
     constructor(fps, gameHost) {
         super(fps, gameHost);
@@ -72,16 +74,84 @@ class SceneTextureAtlas extends SceneMenuBase {
             background.scaleY = 3.5;
             
             // animation
+            var jump_ken = this.addDestroyableObject( this.anims.create({ key: 'JUMP', frames: this.anims.generateFrameNames('KEN-sprite', { prefix: 'JUMP', end: 16, zeroPad: 4 }), duration: 1000, repeat: -1 },) );
             var ani_ken = this.addDestroyableObject( this.anims.create({ key: 'KEN', frames: this.anims.generateFrameNames('KEN-sprite', { prefix: 'KEN', end: 10, zeroPad: 4 }), duration: 1000, repeat: -1 },) );
 
             // play
-            var ken = this.addDestroyableObject( this.add.sprite(this.getSceneWidth() - 20, this.getSceneHeight() - 20, 'character').play('KEN') );
+            var ken = this.addDestroyableObject( this.add.sprite(this.getSceneWidth() - 20, this.getSceneHeight() - 20, 'JANGPUNG').play('KEN') );
             ken.setOrigin(1);
+
+            // jump
+            var jump = this.addDestroyableObject( this.add.sprite(this.getSceneWidth() - 20, this.getSceneHeight() - 160, 'JUMP').play('JUMP') );
+            jump.setOrigin(1);
+
+            //
+            this.#_PV.ani = {
+                ken: ken,
+                jump: {
+                    obj: jump,
+                    orgY: jump.y
+                }               
+            }
 
         } catch(e) {
             var errMsg = this.getKey() + ".onCompleteSerialLoadAllAssets.catched: " + e;
             console.log(errMsg);
             alert(errMsg);
+        }
+    }
+
+    onUpdate() {
+        var ani = this.#_PV.ani;
+        if (ani != undefined) {
+            var frameIndex = parseInt(ani.jump.obj.frame.name.substr(6));
+            //console.log( stringFormat("jump index: {0}", frameIndex) );
+            var jumpY = 0;
+
+            switch (frameIndex)
+            {
+                case 7:
+                    jumpY = 20;
+                    break;
+
+                case 8:
+                    jumpY = 40;
+                    break;
+
+                case 9:
+                    jumpY = 60;
+                    break;
+
+                case 10:
+                    jumpY = 80;
+                    break;
+
+                case 11:
+                    jumpY = 100;
+                    break;
+
+                case 12:
+                    jumpY = 80;
+                    break;
+
+                case 13:
+                    jumpY = 60;
+                    break;
+
+                case 14:
+                    jumpY = 40;
+                    break;
+
+                case 15:
+                    jumpY = 20;
+                    break;
+
+                default:
+                    jumpY = 0;
+                    break;
+            }
+
+            ani.jump.obj.y = ani.jump.orgY - jumpY;
         }
     }
 }
